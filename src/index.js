a512e15b14ac6fa47a0be92a74d62dee844d8fa0
Elise232 <umuhireelise94@gmail.com>
2024-02-18 15:58:58 +0200

    added basic search engine no API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-let searchFormElement = document.querySelector("search-form");
-console.log(searchFormElement);
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let city = document.querySelector("#city");
+    city.innerHTML = searchInput.value;
+}
 
-alert("hello world");
+
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+

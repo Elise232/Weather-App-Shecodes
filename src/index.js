fc021d93021633a30edcd583fe7f88e6ab40c7be
Elise232 <umuhireelise94@gmail.com>
2024-02-18 17:42:56 +0200

    added axios and added the search functionality using the api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+function refreshWeather(response) {
+    let temperature = document.querySelector("#temperature"); 
+    
+    console.log(response.data.temperature.current);
+    
+    
+    
+}
+
+
+
+function searchCity(city) {
+let apiKey ="14caa9243e405tff81o8bbec43672063";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let city = document.querySelector("#city");
     city.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
